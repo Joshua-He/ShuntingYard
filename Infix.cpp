fa7a6f61f0222190358c0abac84b2587bc75ce8f
Joshua-He <joshua5089@gmail.com>
2017-02-24 21:12:01 -0800

    Add files via upload

--- Infix.cpp
+++ Infix.cpp
@@ -2,28 +2,60 @@
 #include <string.h>
 #include <stack>
 #include "Node.h"
+#include <cstring>
 
 using namespace std;
 
-int main(){
 
+void processToken(char * token);
+bool isOperator(char C);
+bool isOperand(char C);
+bool isSpace(char C);
+
+int main(){
+  
   char array[100];
+  char output[100] = "";
+  char token[100]= "";
+  int length;
+  
   cout << "Enter Infix expression" << endl;
-  cin.getline(array);
+  cin.getline(array, 101);
+  length = strlen(array);
+  cout << length << endl;
+  for(int i = 0; i < length; i++){
+    if (array[i] == ' '){
+      cout << token << endl;
+      processToken(token);
+      token[0] = '\0';      
+    }
+    else {
+      strncat(token, &(array[i]), 1);
+    }
+    
+  }
+  cout << token << endl;
+  processToken(token);
   
 }
 
 
-//Convert infix to postfix
+void processToken(char * token){
+  if (isOperator(token[0]) == true){
 
-  for (int i = 0; i <; i++){
-    if (i == 
+  }
+  if (isOperand(token[0]) == true){
+
+  }
+  if (token[0] == '('){
+
+  }
+  if (token[0] == ')'){
+
   }
 }
-
-
 //Evaluate if the character is an operator
-bool isOperator(){
+bool isOperator(char C){
   if (C == '+' || C == '-' || C == '*' || C == '^' || C == '/'){
     return true;
   }
@@ -31,7 +63,7 @@ bool isOperator(){
 }
 
 //Evaluate if the character is a letter or number 
-bool isOperand(){
+bool isOperand(char C){
   if (C >= '0' && C <= '9'){
     return true;
   }
@@ -47,7 +79,7 @@ bool isOperand(){
 }
 
 //evaluate if the character is a space
-bool isSpace(){
+bool isSpace(char C){
   if (C == ' '){
     return true;
   }
--- Node.h
+++ Node.h
@@ -10,7 +10,7 @@ class Node{
 
  private:
   Node* next;
-  char value;
+  char value[20];
 
 
 
